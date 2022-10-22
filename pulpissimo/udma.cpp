2e04abffec56acf3dcb6eb72d26e287db067243e
uzleo <uzleo_eth@proton.me>
2022-10-22 14:26:24 +0000

    SPIM scope in UDMA

--- pulpissimo/udma.cpp
+++ pulpissimo/udma.cpp
@@ -28,7 +28,7 @@ udma::udma(sc_core::sc_module_name nm, scc::memory<512_kB, 32> *l2_mem)
   regs->CTRL_CFG_CG.set_read_cb(vpvper::pulpissimo::simple_read);
   regs->CTRL_CFG_CG.set_write_cb(vpvper::pulpissimo::simple_write);
 
-  spim_regs_cb();
+  spim_.spim_regs_cb();
 }
 
 udma::~udma() {}  // NOLINT
@@ -43,86 +43,102 @@ void udma::reset_cb() {
   }
 }
 
-////////////
-// the callback functions associated with SPI-M registers
-///////////
-void udma::spim_regs_cb() {
+udma::SPIM::SPIM(gen::spi_channel_regs *regs) : regs_{regs} {}
+
+void udma::SPIM::spim_regs_cb() {
   // SPIM_RX_SADDR register
   // right now the UDMA moel is instantaneous i.e. the whole txn data is received atomically from external device
   // hence this reg should always be 0 (possibly by some other part of the model)
-  regs->i_spi.SPIM_RX_SADDR.set_read_cb(vpvper::pulpissimo::simple_read);
-  regs->i_spi.SPIM_RX_SADDR.set_write_cb(
-      [this](scc::sc_register<uint32_t> &reg, uint32_t v, sc_core::sc_time d) -> bool {
-        if (v >= 0x1c000000 && v < 0x1c080000) {
-          reg.put(v);
-          return true;
-        } else {
-          return false;
-        }
-      });
+  regs_->SPIM_RX_SADDR.set_read_cb(vpvper::pulpissimo::simple_read);
+  regs_->SPIM_RX_SADDR.set_write_cb([this](scc::sc_register<uint32_t> &reg, uint32_t v, sc_core::sc_time d) -> bool {
+    if (v >= 0x1c000000 && v < 0x1c080000) {
+      reg.put(v);
+      return true;
+    } else {
+      return false;
+    }
+  });
 
   // SPIM_RX_SIZE register
   // similar to above read_cb of RX_SPIM_SADDR, we expect to return 0 as 'size left' of rx-buffer here
-  regs->i_spi.SPIM_RX_SIZE.set_read_cb(vpvper::pulpissimo::simple_read);
-  regs->i_spi.SPIM_RX_SIZE.set_write_cb(
-      [this](scc::sc_register<uint32_t> &reg, uint32_t v, sc_core::sc_time d) -> bool {
-        if (v <= 1048576) {
-          reg.put(v);
-          return true;
-        } else {
-          return false;
-        }
-      });
+  regs_->SPIM_RX_SIZE.set_read_cb(vpvper::pulpissimo::simple_read);
+  regs_->SPIM_RX_SIZE.set_write_cb([this](scc::sc_register<uint32_t> &reg, uint32_t v, sc_core::sc_time d) -> bool {
+    if (v <= 1048576) {
+      reg.put(v);
+      return true;
+    } else {
+      return false;
+    }
+  });
 
   // SPIM_RX_CFG register
-  regs->i_spi.SPIM_RX_CFG.set_read_cb(vpvper::pulpissimo::simple_read);
-  regs->i_spi.SPIM_RX_CFG.set_write_cb(vpvper::pulpissimo::simple_write);
+  regs_->SPIM_RX_CFG.set_read_cb(vpvper::pulpissimo::simple_read);
+  regs_->SPIM_RX_CFG.set_write_cb(vpvper::pulpissimo::simple_write);
 
   // SPIM_CMD_SADDR register
-  regs->i_spi.SPIM_CMD_SADDR.set_read_cb(vpvper::pulpissimo::simple_read);
-  regs->i_spi.SPIM_CMD_SADDR.set_write_cb(
-      [this](scc::sc_register<uint32_t> &reg, uint32_t v, sc_core::sc_time d) -> bool {
-        if (v >= 0x1c000000 && v < 0x1c080000) {
-          reg.put(v);
-          return true;
-        } else {
-          return false;
-        }
-      });
+  regs_->SPIM_CMD_SADDR.set_read_cb(vpvper::pulpissimo::simple_read);
+  regs_->SPIM_CMD_SADDR.set_write_cb([this](scc::sc_register<uint32_t> &reg, uint32_t v, sc_core::sc_time d) -> bool {
+    if (v >= 0x1c000000 && v < 0x1c080000) {
+      reg.put(v);
+      return true;
+    } else {
+      return false;
+    }
+  });
 
   // SPIM_CMD_SIZE register
-  regs->i_spi.SPIM_CMD_SIZE.set_read_cb(vpvper::pulpissimo::simple_read);
-  regs->i_spi.SPIM_CMD_SIZE.set_write_cb(
-      [this](scc::sc_register<uint32_t> &reg, uint32_t v, sc_core::sc_time d) -> bool {
-        if (v <= 1048576) {
-          reg.put(v);
-          return true;
-        } else {
-          return false;
-        }
-      });
+  regs_->SPIM_CMD_SIZE.set_read_cb(vpvper::pulpissimo::simple_read);
+  regs_->SPIM_CMD_SIZE.set_write_cb([this](scc::sc_register<uint32_t> &reg, uint32_t v, sc_core::sc_time d) -> bool {
+    if (v <= 1048576) {
+      reg.put(v);
+      return true;
+    } else {
+      return false;
+    }
+  });
 
   // SPIM_CMD_CFG register
-  regs->i_spi.SPIM_CMD_CFG.set_read_cb(vpvper::pulpissimo::simple_read);
-  regs->i_spi.SPIM_CMD_CFG.set_write_cb([this](scc::sc_register<uint32_t> &reg, uint32_t v, sc_core::sc_time d) {
+  regs_->SPIM_CMD_CFG.set_read_cb(vpvper::pulpissimo::simple_read);
+  regs_->SPIM_CMD_CFG.set_write_cb([this](scc::sc_register<uint32_t> &reg, uint32_t v, sc_core::sc_time d) {
     reg.put(v);
-    return handleSPIMConfigCommands();
+
+    // make sure that command configurations programmed by CPU are valid before inspecting the CMD buffer
+    bool status{isCMDCFGOk()};
+    if (!status) {
+      return false;
+    }
+    exit(1);
+    // now inspecting the command buffer to update state
+    status = handleCommands();
+    if (!status) {
+      return false;
+    }
+
+    return true;
   });
 }
 
-int udma::handleSPIMConfigCommands() {
-  uint32_t current_cfg{regs->i_spi.SPIM_CMD_CFG.get()};
-  std::cout << std::hex << "current-cfg: " << current_cfg << std::dec << "\n";
-  exit(1);
+bool udma::SPIM::isCMDCFGOk() {
+  printCMDCFG();
+  return true;
+}
 
+void udma::SPIM::printCMDCFG() {
+  gen::spi_channel_regs::SPIM_CMD_CFG_t current_cfg{regs_->SPIM_CMD_CFG.get()};
+  std::cout << "EN = " << current_cfg.EN << "\t";
+  std::cout << "PENDING = " << current_cfg.PENDING << "\t";
+  std::cout << "CLR = " << current_cfg.CLR << "\t";
+  std::cout << "DATASIZE = " << current_cfg.DATASIZE << "\t";
+  std::cout << "CONTINOUS = " << current_cfg.CONTINOUS << "\n";
+}
+
+int udma::SPIM::handleCommands() {
   // 0x0000_0032 : sets the configuration for the SPIM IP t0 0x32 -> clock divided by 50 !! -> HW info not relevant
   // 0x1000_0001 : sets the chip select -> select CSN1 -> HW info not relevant
   // 0x7007_000f : receive data
   // 0x9000_0001 : clear the chip select -> clear CSN1 -> HW info not relevant
-}
 
-//////////
-// the callback functions associated with I2S registers
-//////////
+  return true;
+}
 
 }  // namespace vpvper::pulpissimo
--- pulpissimo/udma.h
+++ pulpissimo/udma.h
@@ -4,8 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef _PULPISSIMO_UDMA_H_
-#define _PULPISSIMO_UDMA_H_
+#pragma once
+
+#include "gen/udma.h"
+//
 #include "scc/memory.h"
 #include "scc/tlm_target.h"
 
@@ -15,6 +17,25 @@ class udma_regs;
 }
 
 class udma : public sc_core::sc_module, public scc::tlm_target<> {
+ protected:
+  class SPIM final {
+   public:
+    SPIM(gen::spi_channel_regs *);
+    void spim_regs_cb();
+    bool isCMDCFGOk();
+    void printCMDCFG();
+    int handleCommands();
+
+   private:
+    const unsigned kTX{0};
+    const unsigned kRX{1};
+    const unsigned kCMD{2};
+
+    gen::spi_channel_regs *regs_{nullptr};
+    // channel-wise
+    std::array<bool, 3> is_enabled_{{false, false, false}};
+  };
+
  public:
   sc_core::sc_in<sc_core::sc_time> clk_i{"clk_i"};
   sc_core::sc_in<bool> rst_i{"rst_i"};
@@ -26,13 +47,10 @@ class udma : public sc_core::sc_module, public scc::tlm_target<> {
   sc_core::sc_time clk;
   std::unique_ptr<gen::udma_regs> regs;
   scc::memory<512_kB, 32> *l2_mem_{nullptr};
+  SPIM spim_{&regs->i_spi};
 
   void clock_cb();
   void reset_cb();
-  void spim_regs_cb();
-  int handleSPIMConfigCommands();
 };
 
 }  // namespace vpvper::pulpissimo
-
-#endif /* _PULPISSIMO_UDMA_H_ */
